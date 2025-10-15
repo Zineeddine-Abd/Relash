4bf22fa9d3c00527aa2114c9d71a5257bca58ebe
XeedOus <100945558+DjeziriOus@users.noreply.github.com>
2025-10-15 14:30:29 +0100

    Suppression Parser du main.

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,36 +1,9 @@
 package Main;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         // Charger config
         DBConfig config = DBConfig.loadFromFile("config/dbconfig.json");
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Relash is live. Tapez vos commandes (EXIT pour quitter)");
-
-        while (true) {
-            System.out.print("> ");
-            String cmd = scanner.nextLine();
-
-            Object parsed = Parser.parse(cmd, config);
-
-            if (parsed == null) {
-                System.out.println("Commande non reconnue !");
-                continue;
-            }
-
-            if (parsed.equals("EXIT")) {
-                System.out.println("Terminated...");
-                break;
-            }
-
-            if (parsed instanceof commandes.CreateTable) {
-                ((commandes.CreateTable) parsed).execute();
-            }
-        }
-
-        scanner.close();
     }
 }
