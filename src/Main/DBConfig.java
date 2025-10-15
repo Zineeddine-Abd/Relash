13da0a90f62c5e13ad763be8acd3bcfb99c17fce
Abdeladim Zine eddine <zineeddine.abdeladim@gmail.com>
2025-10-15 16:47:42 +0200

    modifier un peu le DBConfig

--- src/Main/DBConfig.java
+++ src/Main/DBConfig.java
@@ -6,36 +6,73 @@ import java.io.IOException;
 
 public class DBConfig {
     private final String dbpath;
+    private final int pagesize;
+    private final int dm_maxfilecount;
 
-    public DBConfig(String dbpath) {
+    public DBConfig(String dbpath, int pagesize, int dm_maxfilecount) {
         this.dbpath = dbpath;
+        this.pagesize = pagesize;
+        this.dm_maxfilecount = dm_maxfilecount;
     }
 
     public String getDbPath() {
         return dbpath;
     }
 
+    public int getPageSize() {
+        return pagesize;
+    }
+
+    public int getDmMaxFileCount() {
+        return dm_maxfilecount;
+    }
+
     public static DBConfig loadFromFile(String filename) {
+
         try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
             StringBuilder json = new StringBuilder();
             String line;
+
             while ((line = br.readLine()) != null) {
                 json.append(line.trim());
             }
 
             String content = json.toString();
-            // ex: {"dbpath": "data"}
             content = content.replace("{", "")
                     .replace("}", "")
-                    .replace("\"", "");
-            String[] parts = content.split(":");
-            if (parts.length == 2 && parts[0].trim().equals("dbpath")) {
-                return new DBConfig(parts[1].trim());
+                    .replace("\"", "")
+                    .replace(" ", "");
+
+            String[] pairs = content.split(",");
+            String dbpath = "data";
+            int pagesize = 4096;
+            int dm_maxfilecount = 4;
+
+            for (String pair : pairs) {
+                String[] keyValue = pair.split(":");
+                if (keyValue.length == 2) {
+                    String key = keyValue[0].trim();
+                    String value = keyValue[1].trim();
+
+                    switch (key) {
+                        case "dbpath":
+                            dbpath = value;
+                            break;
+                        case "pagesize":
+                            pagesize = Integer.parseInt(value);
+                            break;
+                        case "dm_maxfilecount":
+                            dm_maxfilecount = Integer.parseInt(value);
+                            break;
+                    }
+                }
             }
+
+            return new DBConfig(dbpath, pagesize, dm_maxfilecount);
 
         } catch (IOException e) {
             System.out.println("Erreur lecture config: " + e.getMessage());
         }
-        return new DBConfig("data");
+        return new DBConfig("data", 4096, 4);
     }
 }
