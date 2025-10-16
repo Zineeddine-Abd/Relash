967673166c7544aeb84dcf83bfce83100bff21d0
Ako-lever <mounirmo0101@gmail.com>
2025-10-16 03:29:36 +0200

    Update run.bat
    Ajout des commentaires dans le fichier run.bat dans la branche Disk_Manager

--- run.bat
+++ run.bat
@@ -1,12 +1,19 @@
 @echo off
-REM Nettoyer ancien bin si existe
+REM -----------------------------
+REM Script pour compiler et exécuter un projet Java
+REM -----------------------------
+
+REM Nettoyer l'ancien dossier 'bin' s'il existe
 if exist bin rmdir /s /q bin
+
+REM Créer un nouveau dossier 'bin'
 mkdir bin
 
-REM
+REM Compiler tous les fichiers Java
 javac -d bin src\*.java src\config\*.java src\commandes\*.java
 
-REM
+REM Exécuter le programme Java
 java -cp bin Main
 
+REM Pause pour garder la fenêtre ouverte
 pause
